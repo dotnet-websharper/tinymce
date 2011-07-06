3e54380dcc14f03824f689eb63c794f1c7b06a76
Joel Bjornson <joel.bjornson@gmail.com>
2011-07-06 18:30:56 +0200

    Add ivy repo for if-ws-tinymce-formlets.

---
+++ if-ws-tinymce/Definitions.fs
@@ -0,0 +1,38 @@
+﻿namespace IntelliFactory.WebSharper.TinyMceExtension
+
+open IntelliFactory.WebSharper.Dom
+
+module TinyMce =
+    open IntelliFactory.WebSharper.InterfaceGenerator
+
+    let TinyMCE = Type.New()
+    let TinyMCEConfiguration = Type.New ()
+
+    let TinyMCEConfigurationClass =
+        Pattern.Config "TinyMCEConfiguration" {
+            Required = []
+            Optional = 
+                [
+                    "mode" , T<string>
+                    "theme" , T<string>
+                ]
+        }
+        |=> TinyMCEConfiguration
+    
+    let TinyMCEClass =
+        Class "tinyMCE"
+        |=> TinyMCE
+        |+> [
+                "init" => TinyMCEConfiguration ^-> T<unit>
+                |> WithComment ""
+                
+            ]
+
+    let Assembly =
+        Assembly [
+            Namespace "IntelliFactory.WebSharper.TinyMce" [
+                TinyMCEConfigurationClass
+                TinyMCEClass
+            ]
+        ]
+
---
+++ test/test/Main.fs
@@ -0,0 +1,31 @@
+﻿namespace IntelliFactory.WebSharper.TinyMce.Test
+
+open IntelliFactory.WebSharper
+open IntelliFactory.WebSharper.Html
+open IntelliFactory.WebSharper.Web
+open IntelliFactory.WebSharper
+
+open IntelliFactory.WebSharper.TinyMce
+
+module Sample =
+    [<JavaScript>]
+    let Init () =
+        JavaScript.Alert "he"
+        TinyMCE.Init (
+            new TinyMCEConfiguration(
+                Theme = "simple",
+                Mode = "textareas"
+            )
+        )
+type SampleControl () =
+    inherit Web.Control()
+
+    [<JavaScript>]
+    override this.Body = 
+        TextArea [Text "Default  text"]
+        |>! OnAfterRender (fun _ ->
+            Sample.Init ()
+        )
+        :> _
+
+

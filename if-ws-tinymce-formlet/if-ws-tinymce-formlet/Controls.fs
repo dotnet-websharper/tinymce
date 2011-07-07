a7a455c1f846b45116b5ef3cd5a44ee98e89062a
Joel Bjornson <joel.bjornson@gmail.com>
2011-07-07 17:17:04 +0200

    Missing ivy file.

--- if-ws-tinymce-formlet/if-ws-tinymce-formlet/Controls.fs
+++ if-ws-tinymce-formlet/if-ws-tinymce-formlet/Controls.fs
@@ -5,15 +5,38 @@
 open IntelliFactory.Formlet
 open IntelliFactory.WebSharper.Formlet
 open IntelliFactory.WebSharper
+open IntelliFactory.WebSharper.TinyMce
 open IntelliFactory.WebSharper.Html
 
 module Controls =
+
+    [<JavaScript>]
+    let (:=) x y : (string * obj) = (x, y)
+
+    [<Inline "void($r[$k]=$v)">]
+    let Set (r: obj) (k: string) (v: obj) : unit = ()
+
+    [<JavaScript>]
+    let (!) (fields: seq<(string * obj)>) =
+        let r = obj ()
+        for (k,v) in fields do
+            Set r k v
+        r
+
     [<JavaScript>]
     let TinyMCE () =
-        Div [Text "Ff"]
-//        Formlet.BuildFormlet <| fun _ ->
-//            // TinyMce.TinyMCE ()
-//            let body = failwith ""
-//            let reset = ignore
-//            let state = failwith ""
-//            body, reset, state
+        Formlet.BuildFormlet <| fun _ ->
+            let body =
+                TextArea []
+                |>! OnAfterRender (fun el ->
+                    let conf =
+                        ![
+                            "onchange_callback" := fun inst -> ()
+                            "mode" := "exact"
+                            "elements" := el.Id
+                        ]
+                    TinyMCE.Init (unbox box conf)
+                )
+            let reset () = ()
+            let state = failwith ""
+            body, reset, state

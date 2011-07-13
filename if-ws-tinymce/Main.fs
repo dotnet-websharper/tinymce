17e82cfb748423f761a874ce6c15e8cb34bac192
Joel Bjornson <joel.bjornson@gmail.com>
2011-07-13 17:58:13 +0200

    Removing resource files.

---
+++ if-ws-tinymce/Main.fs
@@ -0,0 +1,6 @@
+﻿namespace IntelliFactory.WebSharper.TinyMceExtension
+
+module Main =
+    
+    open IntelliFactory.WebSharper.InterfaceGenerator
+    do Compiler.Compile stdout TinyMce.Assembly
---
+++ test/Main.fs
@@ -0,0 +1,16 @@
+﻿namespace IntelliFactory.WebSharper.TinyMce.Test
+
+open IntelliFactory.WebSharper
+open IntelliFactory.WebSharper.Html
+open IntelliFactory.WebSharper.Formlet
+open IntelliFactory.WebSharper.Web
+open IntelliFactory.WebSharper.TinyMce
+open IntelliFactory.WebSharper.Formlet.TinyMce
+
+type SampleControl () =
+    inherit Web.Control()
+
+    [<JavaScript>]
+    override this.Body = 
+        Test.Run()
+        :> _

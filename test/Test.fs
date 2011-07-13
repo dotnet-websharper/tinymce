50f0cbca566f68cff37b23cfeb8b7267f0072ce4
Maciej Lopatka <maciej.lopatka@intellifactory.com>
2011-07-13 17:55:39 +0200

    Finishing testing.

--- test/Test.fs
+++ test/Test.fs
@@ -4,6 +4,7 @@ open IntelliFactory.WebSharper
 open IntelliFactory.WebSharper.Html
 open IntelliFactory.WebSharper.Formlet
 open IntelliFactory.WebSharper.Web
+open IntelliFactory.WebSharper.JQuery
 open IntelliFactory.WebSharper.TinyMce
 open IntelliFactory.WebSharper.Formlet.TinyMce
 
@@ -67,21 +68,109 @@ module Test =
     module DirectBindings =
             
         [<JavaScript>]
-        let SimpleTest() =
+        let CreatingTinyMce() =
+            let Init(tId) =
+                let config = 
+                    new TinyMCEConfiguration (
+                        Theme = "advanced",
+                        Mode = Mode.Exact,
+                        Elements = tId
+                    )
+                TinyMCE.Init(config)
+
+            let tId = NewId()
+            Div [
+                TextArea [Attr.Id tId; Text "default content"]
+                |>! OnAfterRender (fun el ->
+                        Init(tId)
+                    )
+                ]
+
+        [<JavaScript>]
+        let CreatingTinyMceWithOninitCallback() =
             let Init(tId) =
                 let config = 
                     new TinyMCEConfiguration (
                         Theme = "advanced",
                         Mode = Mode.Exact,
                         Elements = tId,
-                        Onchange_callback = (fun editor ->
-                             JavaScript.Alert(editor.GetContent ())
-                        ),
                         
                         Oninit = (fun () ->
-                            let e = TinyMCE.Get(tId)
-                            e.OnKeyUp.Add (fun (editor: Editor) ->
-                                 JavaScript.Alert(editor.GetContent ())
+                            let editor = TinyMCE.Get(tId)
+                            JQuery.Of("#change_on_init").Html("Oninit event executed, editor content: " + editor.GetContent()).Ignore
+                        )
+                    )
+                TinyMCE.Init(config)
+
+            let tId = NewId()
+            Div [
+                TextArea [Attr.Id tId; Text "default content: oninit"]
+                P [Id "change_on_init"]
+                |>! OnAfterRender (fun el ->
+                        Init(tId)
+                    )
+                ]
+
+        [<JavaScript>]
+        let CreatingTinyMceWithOnchangeCallback() =
+            let Init(tId) =
+                let config = 
+                    new TinyMCEConfiguration (
+                        Theme = "advanced",
+                        Mode = Mode.Exact,
+                        Elements = tId,
+                        Onchange_callback = (fun ed ->
+                            JavaScript.Alert(ed.GetContent()) 
+                        )
+                    )
+                TinyMCE.Init(config)
+
+            let tId = NewId()
+            Div [
+                TextArea [Attr.Id tId; Text "default content"]
+                |>! OnAfterRender (fun el ->
+                        Init(tId)
+                    )
+                ]
+
+        [<JavaScript>]
+        let OnKeyupCallback() =
+            let Init(tId) =
+                let config = 
+                    new TinyMCEConfiguration (
+                        Theme = "advanced",
+                        Mode = Mode.Exact,
+                        Elements = tId,
+                        Oninit = (fun () ->
+                            let editor = TinyMCE.Get(tId)
+                            editor.OnKeyUp.Add( fun (ed:Editor) ->
+                                JavaScript.Alert(ed.GetContent()) 
+                            )
+                            |> ignore
+                        )
+                    )
+                TinyMCE.Init(config)
+
+            let tId = NewId()
+            Div [
+                TextArea [Attr.Id tId; Text "default content"]
+                |>! OnAfterRender (fun el ->
+                        Init(tId)
+                    )
+                ]
+
+        [<JavaScript>]
+        let OnClickCallback() =
+            let Init(tId) =
+                let config = 
+                    new TinyMCEConfiguration (
+                        Theme = "advanced",
+                        Mode = Mode.Exact,
+                        Elements = tId,
+                        Oninit = (fun () ->
+                            let editor = TinyMCE.Get(tId)
+                            editor.OnClick.Add( fun (ed:Editor) ->
+                                JavaScript.Alert(ed.GetContent()) 
                             )
                             |> ignore
                         )
@@ -97,6 +186,70 @@ module Test =
                 ]
 
 
+        [<JavaScript>]
+        let UndoManagerUndoAndRedoButtons() =
+            let Init(tId) =
+                let config = 
+                    new TinyMCEConfiguration (
+                        Theme = "advanced",
+                        Mode = Mode.Exact,
+                        Elements = tId
+                    )
+                TinyMCE.Init(config)
+
+            let tId = NewId()
+            Div [
+                TextArea [Attr.Id tId; Text "default content"]
+                |>! OnAfterRender (fun el ->
+                        Init(tId)
+                    )
+                Button [Text "undo"]
+                |>! OnClick (fun el e ->
+                        let undoManager = TinyMCE.Get(tId).UndoManager
+                        undoManager.Undo()
+                        |> ignore
+                    )
+                Button [Text "redo"]
+                |>! OnClick (fun el e ->
+                        let undoManager = TinyMCE.Get(tId).UndoManager
+                        undoManager.Redo()
+                        |> ignore
+                    )
+                ]
+
+
+        [<JavaScript>]
+        let EditorSelectionGetReplace() =
+            let Init(tId) =
+                let config = 
+                    new TinyMCEConfiguration (
+                        Theme = "advanced",
+                        Mode = Mode.Exact,
+                        Elements = tId
+                    )
+                TinyMCE.Init(config)
+
+            let tId = NewId()
+            Div [
+                TextArea [Attr.Id tId; Text "default content"]
+                |>! OnAfterRender (fun el ->
+                        Init(tId)
+                    )
+                Button [Text "get selection"]
+                |>! OnClick (fun el e ->
+                        let selection = TinyMCE.Get(tId).Selection
+                        JavaScript.Alert(selection.GetContent())
+                    )
+                Button [Text "replace selection with foo"]
+                |>! OnClick (fun el e ->
+                        let selection = TinyMCE.Get(tId).Selection
+                        selection.SetContent("foo")
+                    )
+                ]
+
+
+
+    // Test helpers
     
     [<JavaScript>]
     let TestFormlet (name: string)  (descr: string) (formlet: Formlet<string>) =
@@ -114,19 +267,16 @@ module Test =
         ]
     
     [<JavaScript>]
-    let TestDirectBindings (name: string)  (descr: string) (formlet: Formlet<string>) =
+    let TestDirectBindings (name: string)  (descr: string) (element: Element) =
         Div [
             H3 [Text name]
             P [Text descr] 
-            P [
-                Formlet.Do {
-                    let! v = formlet
-                    let! _ = Controls.TextArea v
-                    return ()
-                }
-                |> Enhance.WithFormContainer
-            ]
+            element
         ]
+
+
+
+    // Test runner
 
     [<JavaScript>]
     let Run() =
@@ -144,6 +294,26 @@ module Test =
             TestFormlet "AdvancedHtmlEditor" "Creates AdvancedHtmlEditor with custom configuration: width = 600, height = 400, ToolbarLocation = Top, ToolbarAlign = Left, Plugins = table,contextmenu,paste, Only first row of Buttons = Bold, Anchor"  <|
                 Formlet.AdvancedEditorCustomConfiguration()
 
+
             // Direct bindings tests
-            DirectBindings.SimpleTest()
+            TestDirectBindings "Creating TinyMCE with direct bindings" "TinyMCE should be visible" <|
+                DirectBindings.CreatingTinyMce()
+
+            TestDirectBindings "Creating TinyMCE with direct bindings, Oninit callback" "When TinyMCE is initialized p element below the editor should have the editor's content" <|
+                DirectBindings.CreatingTinyMceWithOninitCallback()
+
+            TestDirectBindings "Creating TinyMCE with direct bindings, Onchange callback" "When TinyMCE content is changed JavaScript Alert dialog is shown with the editor's content" <|
+                DirectBindings.CreatingTinyMceWithOnchangeCallback()
+
+            TestDirectBindings "Creating TinyMCE with direct bindings, OnKeyUp event" "When OnKeyUp event fires JavaScript alert box with editor's content is shown" <|
+                DirectBindings.OnKeyupCallback()
+
+            TestDirectBindings "Creating TinyMCE with direct bindings, OnClick event" "When OnClick event fires JavaScript alert box with editor's content is shown" <|
+                DirectBindings.OnClickCallback()
+
+            TestDirectBindings "Using UndoManager to undo and redo changes" "Buttons below undo and redo changes" <|
+                DirectBindings.UndoManagerUndoAndRedoButtons()
+
+            TestDirectBindings "The editor's selection" "Clicking buttons below gets selection and replaces selected content" <|
+                DirectBindings.EditorSelectionGetReplace()
         ]

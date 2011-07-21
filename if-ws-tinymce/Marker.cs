e7866389b16d54d0e9f8d7c2f3d8b45bfb3133f0
Maciej Lopatka <maciej.lopatka@intellifactory.com>
2011-07-21 17:14:56 +0200

    Changing tiny_mce.js path in Resources.cs.

--- if-ws-tinymce/Marker.cs
+++ if-ws-tinymce/Marker.cs
@@ -1,4 +1,4 @@
 ﻿using System.Web.UI;
 
 [assembly: IntelliFactory.WebSharper.Resources.Require(typeof(IntelliFactory.WebSharper.TinyMce.Resources.TinyMce))]
-//[assembly: WebResource("js/tiny_mce/tiny_mce.js", "text/javascript")]
+//[assembly: WebResource("/js/tiny_mce/tiny_mce.js", "text/javascript")]

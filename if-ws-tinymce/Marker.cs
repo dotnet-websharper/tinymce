cec1345af847b0b4d25f6f845b9dae069695ce2f
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2011-09-19 13:24:13 -0400

    Updated and ran samples on 2.4.

--- if-ws-tinymce/Marker.cs
+++ if-ws-tinymce/Marker.cs
@@ -1,4 +1,3 @@
 ﻿using System.Web.UI;
 
-[assembly: IntelliFactory.WebSharper.Resources.Require(typeof(IntelliFactory.WebSharper.TinyMce.Resources.TinyMce))]
-//[assembly: WebResource("/js/tiny_mce/tiny_mce.js", "text/javascript")]
+[assembly: IntelliFactory.WebSharper.Core.Attributes.Require(typeof(IntelliFactory.WebSharper.TinyMce.Resources.TinyMce))]
